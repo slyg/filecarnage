a7cabf8536a9f0a44ba2ebdaf12af04a90f91fa8
Sylvain Faucherand <syl.faucherand@gmail.com>
2013-02-19 22:16:51 +0100

    changed default parameter to remove spaces

--- schemes.js
+++ schemes.js
@@ -6,13 +6,13 @@ module.exports = {
         type        : String,
         exists      : {type : Boolean, default : true},
         suspicions : {
-            probably_unused         : { type  : String, default : "not scanned" },
-            contains_inline_scripts : { type  : String, default : "not scanned" },
-            may_have_commented_code : { type  : String, default : "not scanned" },
-            contains_$j             : { type  : String, default : "not scanned" },
-            contains_$n$$           : { type  : String, default : "not scanned" },
-            non_native              : { type  : String, default : "not scanned" },
-            probably_unused         : { type  : String, default : "not scanned" }
+            probably_unused         : { type  : String, default : "not_scanned" },
+            contains_inline_scripts : { type  : String, default : "not_scanned" },
+            may_have_commented_code : { type  : String, default : "not_scanned" },
+            contains_$j             : { type  : String, default : "not_scanned" },
+            contains_$n$$           : { type  : String, default : "not_scanned" },
+            non_native              : { type  : String, default : "not_scanned" },
+            probably_unused         : { type  : String, default : "not_scanned" }
         }
     
     }
