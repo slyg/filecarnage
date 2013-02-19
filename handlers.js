3be6104cab826a5ef071a6e72dd5c3abde39ca42
Sylvain Faucherand <syl.faucherand@gmail.com>
2013-02-19 20:22:12 +0100

    errors handling

--- handlers.js
+++ handlers.js
@@ -3,13 +3,23 @@ var
     Hapi    = require('hapi')
 ;
 
+function handleError(req){
+    req.reply(Hapi.Error.internal());
+}
+
 module.exports = {
     retrieve : {
         handler: function(request) {
             if (request.params.id) {
-                Suspect.findOne({_id : request.params.id}, function(err, res){ request.reply(res); });
+                Suspect.findOne({_id : request.params.id}, function(err, res){
+                    if(err) handleError(request); 
+                    request.reply(res);
+                });
             } else {
-                Suspect.find({}, function(err, res){ request.reply(res); });
+                Suspect.find({}, function(err, res){ 
+                    if(err) handleError(request); 
+                    request.reply(res);
+                });
             }
         }
     },
@@ -17,19 +27,19 @@ module.exports = {
         handler: function(request){
             var suspect = new Suspect(request.payload);
             suspect.save(function(){
-                if (err) request.reply(Hapi.Error.internal());
+                if(err) handleError(request); 
                 request.reply(suspect);
             });
         }
     },
     update : {
         handler: function(request){
-        
-            var id = request.params.id;
-            var payload  = request.payload;
-            
+            var 
+                id = request.params.id,
+                payload  = request.payload
+            ;
             Suspect.findOneAndUpdate({ _id: id }, { $set: {suspicions : payload}}).exec(function(err, suspect){
-                if (err) request.reply(Hapi.Error.internal());
+                if(err) handleError(request); 
                 request.reply(suspect);
             });
             
