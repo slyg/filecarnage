02fd8f5bc8dce5b0ca84a7c2cf7490cdeb970096
Sylvain Faucherand <syl.faucherand@gmail.com>
2013-02-21 11:25:25 +0100

    added logo and / is home

--- routes.js
+++ routes.js
@@ -64,7 +64,7 @@ module.exports = function(server){
         },
         {
             method: 'GET',
-            path: '/home',
+            path: '/',
             config: handlers.views.home
         },
         {
