e0371e39df05cac91c4d536b8bf9317f42392064
Sylvain Faucherand <syl.faucherand@gmail.com>
2013-02-21 11:26:29 +0100

    sample conf

---
+++ conf-sample.js
@@ -0,0 +1,10 @@
+module.exports = {
+    api : {
+        host : 'localhost',
+        port : 3000,
+        root : '/myapi'
+    },
+    mongoose : {
+        path : 'mongodb://localhost/stuff'
+    }
+}
