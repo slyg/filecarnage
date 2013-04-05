dd0c11d10e461c6e3209b043892d73e6563f01b4
Sylvain Faucherand <syl.faucherand@gmail.com>
2013-04-05 13:01:37 +0200

    added model (tetra module)

--- schemes.js
+++ schemes.js
@@ -9,20 +9,23 @@ module.exports = {
             removed     : { type : Boolean, default : false },
             scanned     : { type : Boolean, default : false }, // not scanned by default
             suspicions : {
-                probably_unused         : { type  : Boolean, default : true }, // jspfile
-                contains_inline_scripts : { type  : Boolean, default : true }, // jspfile
-                contains_redirect       : { type  : Boolean, default : true }, // jspfile
-                may_have_commented_code : { type  : Boolean, default : true }, // jsfile
-                contains_$j             : { type  : Boolean, default : true }, // jsfile
-                contains_$n$$           : { type  : Boolean, default : true }, // jsfile
-                non_native              : { type  : Boolean, default : true }  // jsObject
+                probably_unused             : { type : Boolean, default : true }, // jspfile
+                contains_inline_scripts     : { type : Boolean, default : true }, // jspfile
+                contains_redirect           : { type : Boolean, default : true }, // jspfile
+                may_have_commented_code     : { type : Boolean, default : true }, // jsfile
+                contains_$j                 : { type : Boolean, default : true }, // jsfile
+                contains_$n$$               : { type : Boolean, default : true }, // jsfile
+                non_native                  : { type : Boolean, default : true }, // jsObject                
+                has_jquery                  : { type : Boolean, default : true }, // tetramodule
+                has_prototype_scriptaculous : { type : Boolean, default : true }, // tetramodule
+                has_other_third_party_dep   : { type : Boolean, default : true }  // tetramodule
             }
         
         }
     
     },
     
-    suspectsTypes : ["jspfile", "jsfile", "jsobject"],
+    suspectsTypes : ["jspfile", "jsfile", "jsobject", "tetramodule"],
     
     suspectSuspicionsPerType : {
         "jspfile"   : [
@@ -37,6 +40,11 @@ module.exports = {
         ],
         "jsobject"  : [
             { ref : 'non_native',                   label : 'Non native'        }
+        ],
+        "tetramodule":[
+            { ref : 'has_jquery',                   label : 'jQuery dep.'       },
+            { ref : 'has_prototype_scriptaculous',  label : 'Prototype/Script. dep.' },
+            { ref : 'has_other_third_party_dep',    label : 'Other dependency'  }
         ]
     }
 }
