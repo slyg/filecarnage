83de0e4d33f60f0123cbd050dbdb3900e3f597a4
Sylvain Faucherand <syl.faucherand@gmail.com>
2013-04-05 11:46:28 +0200

    added new model

--- schemes/index.js
+++ schemes/index.js
@@ -9,20 +9,23 @@ module.exports = {
             removed     : { type : Boolean, default : false },
             scanned     : { type : Boolean, default : false }, // not scanned by default
             suspicions : {
-                probably_unused         : { type  : Boolean, default : true }, // jspfile
-                contains_inline_scripts : { type  : Boolean, default : true }, // jspfile
-                contains_redirect       : { type  : Boolean, default : true }, // jspfile
-                may_have_commented_code : { type  : Boolean, default : true }, // jsfile
-                contains_$j             : { type  : Boolean, default : true }, // jsfile
-                contains_$n$$           : { type  : Boolean, default : true }, // jsfile
-                non_native              : { type  : Boolean, default : true }  // jsObject
+                probably_unused             : { type  : Boolean, default : true }, // jspfile
+                contains_inline_scripts     : { type  : Boolean, default : true }, // jspfile
+                contains_redirect           : { type  : Boolean, default : true }, // jspfile
+                may_have_commented_code     : { type  : Boolean, default : true }, // jsfile
+                contains_$j                 : { type  : Boolean, default : true }, // jsfile
+                contains_$n$$               : { type  : Boolean, default : true }, // jsfile
+                non_native                  : { type  : Boolean, default : true }  // jsObject
+                has_jquery                  : { type  : Boolean, default : true }  // tetramodule
+                has_prototype_scriptaculous : { type  : Boolean, default : true }  // tetramodule
+                has_other_third_party_dep   : { type  : Boolean, default : true }  // tetramodule
             }
         
         }
     
     },
     
-    suspectsTypes : ["jspfile", "jsfile", "jsobject"],
+    suspectsTypes : ["jspfile", "jsfile", "jsobject", "tetramodule"],
     
     suspectSuspicionsPerType : {
         "jspfile"   : [
